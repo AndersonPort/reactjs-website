e91684a241a271b203f87e844f18fe4de994c736
Anderson <andeersonporteela@gmail.com>
2022-01-02 19:14:24 -0300

    Preparing for styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
+
+
+// BEM -> Block Element Modifier method
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='wbs__navbar'>
+            <div className='wbs__navbar-links'>
+                <div className='wbs__navbar-links_logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
         </div>
     )
 }
