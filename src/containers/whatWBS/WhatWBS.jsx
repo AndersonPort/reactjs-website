cf2ef9994d9e35f0395eaeb40da0082975387c75
Anderson <andeersonporteela@gmail.com>
2022-01-11 14:16:09 -0300

    What is gpt3 section

--- src/containers/whatWBS/WhatWBS.jsx
+++ src/containers/whatWBS/WhatWBS.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Feature } from '../../components'
 import './whatWBS.css';
 
 const WhatWBS = () => {
     return (
-        <div>
-            WhatWBS
+        <div className='wbs__whatwbs section__margin' id='whtwbs'>
+            <div className='wbs__whatwbs-feature'>
+                <Feature />
+            </div>
+            <div className='wbs__whatwbs-heading'>
+                <h1 className='gradient__text'>The Possibilities are beyond your imagination</h1>
+                <p>Explore the library</p>
+            </div>
+            <div className='wbs__whatwbs-container'>
+                <Feature />
+                <Feature />
+                <Feature />
+            </div>
         </div>
     )
 }
