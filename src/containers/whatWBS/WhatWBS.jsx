36bcf5c96b20a0317e4f5f0db47e368a9203c119
Anderson <andeersonporteela@gmail.com>
2022-01-12 09:56:38 -0300

    Case studies done and some navigation bugs fixed

--- src/containers/whatWBS/WhatWBS.jsx
+++ src/containers/whatWBS/WhatWBS.jsx
@@ -4,7 +4,7 @@ import './whatWBS.css';
 
 const WhatWBS = () => {
     return (
-        <div className='wbs__whatwbs section__margin' id='whtwbs'>
+        <div className='wbs__whatwbs section__margin' id='whtgpt3'>
             <div className='wbs__whatwbs-feature'>
                 <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
             </div>
