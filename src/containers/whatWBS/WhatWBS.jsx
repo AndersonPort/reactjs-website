2f8efcaff6dece8f2ac4dbd0c60e6ac4baadc636
Anderson <andeersonporteela@gmail.com>
2022-01-11 23:02:55 -0300

    What is GPT3 section done

--- src/containers/whatWBS/WhatWBS.jsx
+++ src/containers/whatWBS/WhatWBS.jsx
@@ -6,16 +6,16 @@ const WhatWBS = () => {
     return (
         <div className='wbs__whatwbs section__margin' id='whtwbs'>
             <div className='wbs__whatwbs-feature'>
-                <Feature />
+                <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."/>
             </div>
             <div className='wbs__whatwbs-heading'>
                 <h1 className='gradient__text'>The Possibilities are beyond your imagination</h1>
                 <p>Explore the library</p>
             </div>
             <div className='wbs__whatwbs-container'>
-                <Feature />
-                <Feature />
-                <Feature />
+                <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
+                <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+                <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
             </div>
         </div>
     )
