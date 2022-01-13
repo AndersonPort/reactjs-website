3c47421b6d597e9c193053696bcfe415dfd72715
Anderson <andeersonporteela@gmail.com>
2022-01-13 09:23:30 -0300

    CTA section done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const CTA = () => {
     return (
-        <div>
-            CTA
+        <div className='wbs__cta'>
+            <div className='wbs__cta-content'>
+                <p>Request Early Access to Get Started</p>
+                <h3>Register today & start exploring the endless possibilities.</h3>
+            </div>
+            <div className='wbs__cta-btn'>
+                <button type='button'>Get Started</button>
+            </div>
         </div>
     )
 }
